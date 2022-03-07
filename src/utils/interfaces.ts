1982d925a77452ecaa987d2ae71cfa1a07fb3d6f
Milan Camus <pro@turtletv.fr>
2022-03-07 23:12:46 +0100

    feat: ✨ Add new interfaces for `TypeScript`

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -12,3 +12,26 @@ export interface SQLiteQueryParams {
 export interface SQLiteSelectParams {
   query: string;
 }
+
+export interface MySQLResponse {
+  fieldCount: number;
+  affectedRows: number;
+  insertId: number;
+  info: string;
+  serverStatus: number;
+  warningStatus: number;
+}
+
+/**
+ * USER RELATED INTERFACES
+ */
+
+export interface User {
+  id?: number;
+  uuid: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  role: string;
+  token?: string;
+}
