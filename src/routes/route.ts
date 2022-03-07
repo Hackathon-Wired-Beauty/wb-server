6f3ac6c0066d1aac281103421da4ef5e3f014be6
Milan Camus <pro@turtletv.fr>
2022-03-08 00:09:39 +0100

    feat: ✏️ Update the test route

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,10 +1,16 @@
 import { db } from "../db";
+import { authorization } from "../middleware/authorization";
 import { Res, returnCode } from "../utils/returnCodes";
 const env = require("dotenv").config();
 
+const { auth } = require("../middleware/auth");
+
 const route = (app: any) => {
-  app.post("/route", async function (req: any, res: Res) {
-    // TODO
+  app.post("/route", auth, authorization, async function (req: any, res: Res) {
+    const { user } = req;
+    res
+      .status(200)
+      .json({ route: { version: process.env.VERSION, user: user } });
   });
 };
 
