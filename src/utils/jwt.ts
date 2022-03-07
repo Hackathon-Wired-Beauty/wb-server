a7286220b74583caf227cb45b9be0d8daf5d544b
Milan Camus <pro@turtletv.fr>
2022-03-07 23:12:11 +0100

    feat: ✨ Add utils functions

---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+const env = require("dotenv").config();
+import { User } from "./interfaces";
+const jwt = require("jsonwebtoken");
+
+export const generateToken = (user: User): string => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      uuid: user.uuid,
+      first_name: user.firstname,
+      last_name: user.lastname,
+      email: user.email,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRATION,
+    }
+  );
+  return token;
+};
